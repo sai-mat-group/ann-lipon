15133be44f12b7ae94a38b83992bcc9ca8bf2084
Rutvij12 <f20170830@hyderabad.bits-pilani.ac.in>
2021-03-05 16:56:45 +0530

    calculated bulk structures and slab structures

---
+++ BaseStructures/Input_files_mp_28450/job_script_quick.sh
@@ -0,0 +1,26 @@
+# ---------------------------
+#PBS -N test_job 
+#PBS -q debug
+#PBS -l nodes=1
+#PBS -l walltime=01:59:00
+#PBS -j oe
+#PBS -V 
+#PBS -m abe 
+#PBS -M saigautamg@iisc.ac.in
+#PBE -o job.out
+#PBE -e error
+#---------------------------
+
+#Possible queue options: debug (1-5 nodes, max. 2h), small72 (1-3 nodes; max. 72h), medium (3-5 nodes, max. 48h)
+#Below are node specifications
+#CPU ONLY : nodes=n:ppn=24:cpu24a
+#XEON PHI : nodes=1:ppn=24:xeonphi
+#HIGH RAM : nodes=1:ppn=24:hm512g
+
+module load compiler/intel/2018
+
+cd $PBS_O_WORKDIR
+
+VASP_EXEC="/sscu_gpfs/home/gautam/bin/vasp.5.4.4/exec/vasp_std"
+mpirun -n 24 ${VASP_EXEC} >& output
+
