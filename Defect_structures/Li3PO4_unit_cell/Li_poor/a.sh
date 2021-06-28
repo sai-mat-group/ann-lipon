72ccde73c90f09d9fd6251fbc668b5f68babf1a4
Rutvij12 <f20170830@hyderabad.bits-pilani.ac.in>
2021-06-28 13:20:25 +0530

    from local desktop

--- Defect_structures/Li3PO4_unit_cell/Li_poor/a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {22..89}
-do
-	cd /home/smart/ann-lipon/Defect_structures/Li3PO4_unit_cell/Li_poor/Li5P2N1O6_${i}
-	rm EIGENVAL IBZKPT KPOINTS OSZICAR output PCDAT POTCAR REPORT INCAR transformations.json
-	cd ../
-done
-
