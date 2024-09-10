bd75531773de01024aab8b9aaa25299e46d87d67
Aqustrii <f20190660h@alumni.bits-pilani.ac.in>
2024-09-10 23:05:24 +0530

    Reuploading Structures

--- Hydrostatic_strain/Batch1/Input_files_Hydrostatic_-9_strain_mp_736/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-module load vasp/6.1.2
-mpirun -n 8 vasp_std >& output
--- Hydrostatic_strain/Batch9/Input_files_Hydrostatic_-1_strain_mp_9588/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load vasp/6.1.2
-mpirun -n 12 vasp_std >& output
-wait
-rm CHG CHGCAR DOSCAR PROCAR vasprun.xml WAVECAR XDATCAR
-bzip2 -z OUTCAR
-
